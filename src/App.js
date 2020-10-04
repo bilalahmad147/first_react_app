2a05c1ab8093d1a2d294584a8638f81612cb5d6d
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-10-04 12:59:08 +0500

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Hello from "./Components/Hello"
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Hello />
+      <Welcome />
     </div>
   );
 }
