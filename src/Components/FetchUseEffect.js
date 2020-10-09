d7116c8d1dca582bc76f45a3acbccea24e7899be
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-10-10 02:21:12 +0500

    Update FetchUseEffect.js

--- src/Components/FetchUseEffect.js
+++ src/Components/FetchUseEffect.js
@@ -1,27 +1,34 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 function FetchUseEffect() {
 
     const [Question, setQuestion] = useState([])
 
-    const fetchData = async() => {
+    const fetchData = async () => {
         const response = await fetch("https://opentdb.com/api.php?amount=5");
         const data = await response.json();
         const result = data.results
-        console.log(result)
+        console.log(data)
         setQuestion(result)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData()
-    },[]);
+    }, []);
 
     return (
         <div>
             <h1>Questions</h1>
             {
-                Question.map((obj,ind)=>{
-                return <li key={ind}>{obj.question}</li>
+                Question.map((obj, ind) => {
+                    return (
+                        <div key={ind}>
+                            <h2>{obj.question}</h2>
+                            <ul>
+                                <li>{obj.incorrect_answers}{obj.correct_answer}</li>
+                            </ul>
+                        </div>
+                    )
                 })
             }
         </div>
