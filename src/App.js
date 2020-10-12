5fc2ce4db3c85cecace0fb5fc28f701421c3672b
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-10-12 17:24:53 +0500

    react router install and used

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
 import FetchUseEffect from './Components/FetchUseEffect';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
 
 
 function App() {
@@ -8,7 +16,12 @@ function App() {
   return (
       <div className="App">
         <FetchUseEffect />
+        <Router>
+          <Route path='/home'><Home /></Route>
+          <Route path='/about'><About /></Route>
+        </Router>
       </div>
+      
   );
 }
 
