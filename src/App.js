0b2ad4b13f9cde20cd82e81ce34f7c82696e24fe
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-10-14 22:09:19 +0500

    install animation library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import './App.css';
-import FetchUseEffect from './Components/FetchUseEffect';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from './Components/Home';
-import About from './Components/About';
+import Animation from './Components/Animation'
 
 
 function App() {
 
   return (
       <div className="App">
-        <FetchUseEffect />
-        <Router>
-          <Route path='/home'><Home /></Route>
-          <Route path='/about'><About /></Route>
-        </Router>
+        <Animation />
       </div>
       
   );
