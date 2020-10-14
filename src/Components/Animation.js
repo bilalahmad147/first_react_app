058d7d9fa75f3516dd01f6320cb66405bc9dd9c2
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-10-14 22:26:40 +0500

    updated

--- src/Components/Animation.js
+++ src/Components/Animation.js
@@ -4,22 +4,7 @@ import useWebAnimations from "@wellyshen/use-web-animations";
 
 function Animation() {
 
-    const { ref, playState } = useWebAnimations({
-        keyframes: {
-            transform: ["translateX(500px)"], // Move by 500px
-            background: ["red", "blue", "green"], // Go through three colors
-        },
-        timing: {
-            delay: 500, // Start with a 500ms delay
-            duration: 1000, // Run for 1000ms
-            iterations: Infinity, // Repeat once
-            direction: "alternate", // Run the animation forwards and then backwards
-            easing: "ease-in-out", // Use a fancy timing function
-        },
-        onReady: ({ playState, animate, animation }) => { },
-        onUpdate: ({ playState, animate, animation }) => { },
-        onFinish: ({ playState, animate, animation }) => { },
-    });
+    const { ref } = useWebAnimations({...animate__tada})
 
     return (
         <div>
